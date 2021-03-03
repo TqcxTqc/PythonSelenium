85b0837688b4ec690c55c469f2d234cfed7b7e25
tqcxtqc <vitali.dambajev@insly.com>
2021-03-03 22:11:42 +0200

    Test catalogue moved as PageObject

--- tests/test_catalogue_page.py
+++ tests/test_catalogue_page.py
@@ -1,10 +1,6 @@
 import pytest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import random
-
-CATALOGUE_URL = "/index.php?route=product/category&path=20"
+from _support.page import ProductPage
 
 
 @pytest.mark.parametrize('expected_item_titles',
@@ -13,37 +9,24 @@ CATALOGUE_URL = "/index.php?route=product/category&path=20"
                            'Product 8', 'Samsung SyncMaster 941BW', 'Sony VAIO']])
 def test_desktops_catalogue(browser, expected_item_titles):
     """Checking desktops products from catalogue page"""
-    browser.get(f"{browser.url}{CATALOGUE_URL}")
-    get_items_titles = browser.find_elements_by_css_selector(".caption a")
-    actual_titles = []
-
-    for item in get_items_titles:
-        actual_titles.append(item.text)
-
-    assert actual_titles == expected_item_titles
+    product_page = ProductPage(browser)
+    product_page.open_special_page(ProductPage.CATALOGUE_URL)
+    product_page.get_all_items_title_on_page(expected_item_titles)
 
 
 def test_desktop_filter(browser):
     """Checking filter for desktops products by Mac"""
-    browser.get(f"{browser.url}{CATALOGUE_URL}")
-    wait = WebDriverWait(browser, 5)
-    left_menu = browser.find_element_by_css_selector("#column-left")
-    mac_item = left_menu.find_element_by_css_selector(".list-group-item:nth-child(3)")
-
-    mac_item.click()
-    wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".caption h4"), "iMac"))
+    product_page = ProductPage(browser)
+    product_page.open_special_page(ProductPage.CATALOGUE_URL)
+    product_page.select_item_in_left_menu('Mac')
+    product_page.check_item_to_be_present('iMac')
 
 
 def test_add_comparison_products(browser):
     """Checking of adding product in to link 'Product Compare'"""
-    browser.get(f"{browser.url}{CATALOGUE_URL}")
-    wait = WebDriverWait(browser, 5)
+    product_page = ProductPage(browser)
+    product_page.open_special_page(ProductPage.CATALOGUE_URL)
     random_item = random.randint(1, 5)
-    product_compare_link = browser.find_element_by_id("compare-total")
-
-    wait.until(EC.element_to_be_clickable(
-        (By.XPATH, f"(//div[@class='button-group']/button[contains(@onclick,'compare.add')])[{random_item}]"))).click()
-
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".alert-success")))
-
-    assert product_compare_link.text == "Product Compare (1)"
+    product_page.check_compare_link(0)
+    product_page.click_comparison_button(random_item)
+    product_page.check_compare_link(1)
